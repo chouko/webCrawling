ac3c9e504b461ba43fac9666fcbe297f77d70d96
yuting.d.zhang <chyouko_zhang@hotmail.com>
2021-06-30 18:53:27 +0800

    [add] 公众号爬虫功能。

--- wechat/wechat/pipelines.py
+++ wechat/wechat/pipelines.py
@@ -2,8 +2,11 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime
+import time
+
 from common.pipelines import base_pipelines
 
 
 class WechatPipeline(base_pipelines.JsonBasePipeline):
-    file_name = '/wechat-list'
+    file_name = '/' + time.strftime("%Y%m%d%H%M%S", time.localtime())
--- wechat/wechat/spiders/list_spider.py
+++ wechat/wechat/spiders/list_spider.py
@@ -17,7 +17,7 @@ class ListSpider(Spider):
     def __init__(self, name=None, **kwargs):
         super().__init__(name, **kwargs)
         self.__search = listsearch.ListSearch()
-        self.__index = 295
+        self.__index = 0
 
     def start_requests(self):
         while True:
